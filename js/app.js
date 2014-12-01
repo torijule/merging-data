c531fc686b47669c208408d89ae94bc013a1a535
torijule <torijule@gmail.com>
2014-12-01 01:33:05 -0800

    Mostly finished
    Still needs active attribute added.  Cards populate and sort., including
    reverse on double click.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+"use strict";
+var addressBook = angular.module('AddressBook', []);
+
+addressBook.controller('AddressController', ['$scope', function($scope){
+	$scope.employees = pawneeEmployees;
+	$scope.order = 'lastName';
+	
+
+}]);
